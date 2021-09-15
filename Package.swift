40a6cf7e1bd253bac7bd825914bf13d8227b0a26
David Šafarik <dsafarik@csas.cz>
2021-09-15 12:41:30 +0200

    DSA: url and checksum update

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.binaryTarget(name: "AWSDK",
-					  path: "AWSDK.xcframework.zip")
+					  url: "https://github.com/lukassurin-ext96070/airwatch-test/blob/master/AWSDK.xcframework.zip",
+					  checksum: "c60ee24d3f3ec8032bcd5203df3d2b81f8456d33c769b900a5e2048e85501153")
 	]
 )
